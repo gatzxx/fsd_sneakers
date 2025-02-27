6e07f666351a99fc4d23bdc6bd0c470e381dba36
Egor K <murwoofmur@gmail.com>
2025-02-27 11:15:23 +0700

    refactor(theme): improve ThemeProvider

--- src/app/providers/theme/ThemeProvider.tsx
+++ src/app/providers/theme/ThemeProvider.tsx
@@ -5,18 +5,15 @@ import { Theme } from '@/shared/config/theme/theme'
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
     const getStoredTheme = (): Theme => {
-        return (localStorage.getItem(THEME_LOCALSTORAGE_KEY) as Theme) || Theme.LIGHT
+        const stored = localStorage.getItem(THEME_LOCALSTORAGE_KEY)
+        return stored === Theme.LIGHT || stored === Theme.DARK ? stored : Theme.LIGHT
     }
 
-    const setStoredTheme = (theme: Theme): void => {
-        localStorage.setItem(THEME_LOCALSTORAGE_KEY, theme)
-    }
-
-    const [theme, setTheme] = useState<Theme>(getStoredTheme())
+    const [theme, setTheme] = useState<Theme>(() => getStoredTheme())
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme)
-        setStoredTheme(theme)
+        localStorage.setItem(THEME_LOCALSTORAGE_KEY, theme)
     }, [theme])
 
     return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>
